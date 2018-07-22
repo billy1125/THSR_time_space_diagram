d3ef2410d06f8236ecbf8f4d6b98ec7d5c1bc59e
billy1125 <billy1125@gmail.com>
2018-07-22 21:53:02 +0800

    Initial commit

---
+++ diagram.py
@@ -0,0 +1,261 @@
+import json
+
+import pandas as pd # 引用套件並縮寫為 pd
+import numpy as np
+
+#自訂class與module
+import basic_data
+
+
+#處理所有車站基本資訊(Stations.csv)
+stations = basic_data.stations()
+
+#時間轉換(Locate.csv)
+time_loc = basic_data.time_loc()
+
+#讀取台鐵JSON
+def read_json(filename):
+    with open('JSON/' + filename, 'r', encoding='utf8') as data_file:    
+        data = json.load(data_file)
+    
+    return data
+
+#找出每一個車次
+def find_trains(data, train_no):
+
+    trains = []
+
+    if train_no == '':
+        for x in data: #逐車次搜尋
+            trains.append(x)
+    # elif train_no != '':
+    #     for x in data['TrainInfos']: #逐車次搜尋
+    #         if x['Train'] == train_no:
+    #             trains.append(x)
+
+    return trains
+
+#找出每一個車次的表定經過車站
+def find_train_stations(train_no):
+
+    list_start_end_station = {}
+    
+    for station in train_no['StopTimes']:
+        list_start_end_station[station['StationID']] = [station['ArrivalTime'], station['DepartureTime'], station['StationID'], station['StopSequence']]
+
+    return list_start_end_station
+
+    
+#找出每一個車次所有會經過的車站，無論是否會停靠
+def find_stations(list_start_end_station, line, line_dir):
+
+    #起終點車站代碼
+    end_station_number = len(list_start_end_station) - 1
+    start_station = list(list_start_end_station)[0]
+    end_station = list(list_start_end_station)[end_station_number]
+
+    global stations
+
+    temp = []
+    station = start_station
+
+    km = 0.0 #計算經過車站里程
+        
+    while True:
+
+        temp.append([stations[station][0], stations[station][1], stations[station][3], km])
+        # print(stations[station][1])
+        if line_dir == 0:  #南下
+
+            km += float(stations[station][10])
+            station = stations[station][4]
+
+        elif line_dir == 1:  #北上
+
+            km += float(stations[station][11])
+            station = stations[station][5]
+
+        if station == end_station:
+            temp.append([stations[station][0], stations[station][1], stations[station][3], km])
+            break
+
+        if len(temp) > 200:
+            print(len(temp))
+            break
+            
+    list_passing_stations = temp
+
+    return list_passing_stations
+
+#將所有經過車站找出，並且將通過車站的時間點估計出來
+def train_time_to_stations(list_start_end_station, list_passing_stations):
+    
+    global time_loc
+
+    station = []
+    station_id = []
+    time = []
+    loc = []
+
+
+
+    for item in list_passing_stations:
+
+        station_id.append(item[0])
+        station.append(item[1])
+        loc.append(item[3])
+
+        if list_start_end_station.__contains__(item[0]): #如果經過車站清單中包括停靠車站，則將出發時間再加入
+            #list_passing_stations[index].append(int(list_station[item[0]].replace(':','')))
+            ArrTime = int(time_loc[list_start_end_station[item[0]][0]])
+            DepTime = int(time_loc[list_start_end_station[item[0]][1]])
+
+            time.append(ArrTime)
+
+            if ArrTime > DepTime: #車站內跨午夜處理，將跨午夜車站通過時間增加1440與0，之後在svg_save重複繪製兩次
+                station_id.append('-1')
+                station.append('跨午夜')
+                loc.append(item[3])
+                time.append(1440)
+
+                station_id.append('-1')
+                station.append('跨午夜')
+                loc.append(item[3])
+                time.append(0)
+
+            if item[0] == 'End':
+                station_id.append(list_passing_stations[0][0])
+            else:
+                station_id.append(item[0])
+            station.append(item[1])
+            loc.append(item[3])
+            time.append(DepTime)
+        else:
+            #list_passing_stations[index].append(np.NaN)
+            time.append(np.NaN)
+
+    dict = {"Station": station, "Time": time, "Loc": loc, "Station ID": station_id}
+    
+    select_df = pd.DataFrame(dict)
+    #select_df = select_df.sort_values(by = 'Loc')
+    
+    select_df = select_df.set_index('Loc').interpolate(method='index') #估計通過時間
+
+    return select_df
+
+
+#跨午夜車次處理，基本邏輯，非車站內跨午夜則必須估計出午夜十二點的位置
+def midnight_train(list_start_end_station, list_passing_stations, over_night_stn):
+    
+    global time_loc
+
+    nidmight_km = 0
+
+    midnight_in_station = False
+
+    station = []
+    station_id = []
+    time = []
+    loc = []
+
+    i = 0
+
+    while True:
+        item = list_passing_stations[i]
+        if list_start_end_station.__contains__(item[0]):
+            ArrTime = int(time_loc[list_start_end_station[item[0]][0]])
+            DepTime = int(time_loc[list_start_end_station[item[0]][1]])
+                
+            if item[0] == over_night_stn:
+
+                if DepTime >= ArrTime: #插入一個跨午夜的虛擬車站，藉此估計列車所在的里程數
+                    station_id.append('-1')
+                    station.append('跨午夜')
+                    loc.append(np.NaN)
+                    time.append(1440)
+
+                    station_id.append(item[0])
+                    station.append(item[1])
+                    loc.append(float(item[3]))
+                    time.append(ArrTime + 1440) #要將跨午夜車站的時間加上1440估計較為適當
+
+                elif DepTime < ArrTime: #車站內跨日則跳過處理，於train_time_to_stations函數進行處理即可
+                    midnight_in_station = True
+                    
+            else:
+                station_id.append(item[0])
+                station.append(item[1])
+                loc.append(float(item[3]))
+                time.append(ArrTime)
+
+                station_id.append(item[0])
+                station.append(item[1])
+                loc.append(float(item[3]))
+                time.append(DepTime)
+
+        i += 1
+        if item[0] == over_night_stn:
+            break
+
+    dict = {"Station": station, "Time": time, "Loc": loc, "Station ID": station_id}
+
+    select_df = pd.DataFrame(dict)
+    # print(select_df)
+    select_df = select_df.interpolate(method='index') #估計午夜通過里程
+    
+    # print(select_df[select_df.loc[:,"Station ID"] == '-1'].iloc[0, 0])
+
+    if midnight_in_station == False: #如果跨午夜車次不是在車站內跨夜，才將資料帶出
+        nidmight_km = select_df[select_df.loc[:,"Station ID"] == '-1'].iloc[0, 0]
+
+    return nidmight_km
+
+#環島列車處理，例如：51、52車次，直接拆成兩條路線處理，以八堵車站、竹南車站為分界點
+def roundabout_train(train_time_space, line_dir):
+    output = []
+    row_number = -1
+
+    number1 = ''
+    number2 = ''
+
+    if line_dir == '0':
+        number1 = '1002'  # 八堵
+        number2 = '1028'  # 竹南
+    elif line_dir == '1':
+        number1 = '1028'
+        number2 = '1002'
+
+    train_time_space['Loc'] = train_time_space.index
+
+    for i in range(0, 3):
+        station = []
+        station_id = []
+        time = []
+        loc = []
+        temp = []
+        if i in [1, 2]:
+            row_number -= 1
+        while True:
+            row_number += 1
+            temp.append(train_time_space.iloc[row_number])
+
+            station_id.append(train_time_space.iloc[row_number, 1])
+            station.append(train_time_space.iloc[row_number, 0])
+            loc.append(train_time_space.iloc[row_number, 3])
+            time.append(train_time_space.iloc[row_number, 2])
+
+            if i == 2 and row_number == len(train_time_space) - 1:
+                break
+            if i == 0 and train_time_space.iloc[row_number, 1] == number1:
+                break
+            if i == 1 and train_time_space.iloc[row_number, 1] == number2:
+                break
+
+        dict = {"Station": station, "Time": time, "Loc": loc, "Station ID": station_id}
+
+        select_df = pd.DataFrame(dict)
+        select_df = select_df.set_index('Loc').interpolate(method='index')  # 估計通過時間
+
+        output.append(select_df)
+
+    return output
---
+++ svg_save.py
@@ -0,0 +1,189 @@
+#運行圖SVG繪製
+import csv
+import svgwrite
+import time
+import pandas as pd # 引用套件並縮寫為 pd
+import numpy as np
+
+# dwg = None
+# stations_to_draw = []
+# stations_loc = {}
+
+dict_car_kind = {
+    '1131': 'local',
+    '1132': 'local',
+    '1100': 'tze_chiang_diesel',
+    '1101': 'tze_chiang',
+    '1102': 'taroko',
+    '1103': 'tze_chiang_diesel',
+    '1107': 'puyuma',
+    '1108': 'tze_chiang',
+    '1109': 'tze_chiang',
+    '110A': 'tze_chiang',
+    '110B': 'emu1200',
+    '110C': 'emu300',
+    '110D': 'tze_chiang_diesel',
+    '110E': 'tze_chiang_diesel',
+    '110F': 'tze_chiang_diesel',
+    '1110': 'chu_kuang',
+    '1111': 'chu_kuang',
+    '1114': 'chu_kuang',
+    '1115': 'chu_kuang',
+    '1120': 'fu_hsing',
+    '1140': 'ordinary',
+    '0000': 'special'
+    }
+
+dict_line_kind = {
+    'LINE_WN': '西部幹線北段（基隆-竹南）',
+    'LINE_WM': '西部幹線台中線（竹南-彰化，經苗栗）',
+    'LINE_WSEA': '西部幹線海岸線（竹南-彰化，經大甲）',
+    'LINE_WS': '西部幹線南段（彰化-高雄）',
+    'LINE_P': '屏東線（高雄-枋寮）',
+    'LINE_S': '南迴線（枋寮-台東）',
+    'LINE_T': '台東線（花蓮-台東）',
+    'LINE_N': '北迴線（蘇澳新-花蓮）',
+    'LINE_I': '宜蘭線（八堵-蘇澳）',
+    'LINE_PX': '平溪深澳線（八斗子-菁桐）',
+    'LINE_NW': '內灣線（新竹-內灣）',
+    'LINE_J': '集集線（二水-車埕）',
+    'LINE_SL': '沙崙線（中洲-沙崙',
+    'LINE_THSR': '台灣高鐵',
+}
+
+class Draw:
+    def __init__(self, location, date, line, version, height):
+        self.location = location
+        self.date = date
+
+        filename = ''
+        if location == '':
+            filename = 'OUTPUT/' + date + '.svg'
+        else:
+            filename = location + date + '.svg'
+        self.file_name = filename
+        self.line = line
+        
+
+        self.dwg = None
+        self.stations_to_draw = []
+        self.stations_loc = {}
+
+        self.height = height
+
+        #背景、基本大小
+        self.dwg = svgwrite.Drawing(self.file_name, size = (14500, self.height + 100), profile='full', style='background:#b3fff0;font-family:Tahoma')
+        self.dwg.add_stylesheet('style.css', title="sometext") 
+        #處理所有車站基本資訊(Category.csv)
+        with open('CSV/Category.csv', newline='', encoding='utf8') as csvfile:
+            
+            reader = csv.reader(csvfile)
+            
+            for row in reader:
+                if row[0] == self.line:
+                    self.stations_to_draw.append(row)
+                    if row[1] != 'NA':
+                        self.stations_loc[row[1]] = float(row[3])
+
+        self.draw_background(version)
+
+    #繪製基底圖
+    def draw_background(self, version):
+        localtime = time.asctime(time.localtime(time.time()))
+
+        self.dwg.add(self.dwg.text(dict_line_kind[self.line] + ' 日期：' + self.date +'，運行圖均來自交通部MOTC公開資料所分析，僅供參考，正確資料與實際運轉狀況請以高鐵網站或公告為主。台灣高鐵Open Data轉檔運行圖程式版本：' + version + ' 轉檔時間：' + localtime, insert=(5, 20), fill='#000000'))
+
+        #時間線
+
+        #小時
+        for i in range(0, 25):
+            x = 50 + i * 600
+            self.dwg.add(self.dwg.line((x, 50), (x, self.height + 50), class_='hour_line'))
+            for j in range(0, 11):
+                self.dwg.add(self.dwg.text('{:0>2d}'.format(i) + '00', insert=(x, 49 + j * 300), fill='#999966'))
+            #每10分鐘
+            if i != 24:
+                for j in range(0, 5):
+                    x = 50 + i * 600 + (j + 1) * 100
+                    if j != 2:
+                        self.dwg.add(self.dwg.line((x, 50), (x, self.height + 50), class_='min10_line'))
+                    else: #30分鐘顏色不同
+                        self.dwg.add(self.dwg.line((x, 50), (x, self.height + 50), class_='min30_line'))
+                        for k in range(0, 11):
+                            self.dwg.add(self.dwg.text('30', insert=(x, 49 + k * 300), fill='#999966'))
+
+        #車站線
+        #dwg.add(dwg.line((50, 50), (14450, 50), style=style_01))
+
+        for item in self.stations_to_draw:
+            y = float(item[3]) + 50
+            if item[1] != 'NA':
+                self.dwg.add(self.dwg.line((50, y), (14450, y), class_='station_line'))
+            else:
+                self.dwg.add(self.dwg.line((50, y), (14450, y), class_='station_noserv_line'))
+                
+            for i in range(0, 25):
+                if item[1] != 'NA':
+                    self.dwg.add(self.dwg.text(item[2], insert=(5 + i * 600, y - 5), fill='#000000'))
+                else:
+                    self.dwg.add(self.dwg.text(item[2], insert=(5 + i * 600, y - 5), fill='#bfbfbf'))
+
+        
+
+    #繪製車次線
+    def draw_trains(self, train_time_space, train_id, car_class, line):
+        
+        to_count_stations = True
+        check_number = 0
+        midnight = -1
+        color = dict_car_kind[car_class]
+        midnight_loc = -1
+        cheng_zhui_passing = {'1321': 0, '1118': 0}
+        cheng_zhui_local = False
+
+        if to_count_stations == True:
+            for i in range(0, len(train_time_space.index)):
+                if self.stations_loc.__contains__(train_time_space.iloc[i, 1]):
+                    check_number += 1 #確認資料有超過兩筆
+
+        
+        if cheng_zhui_local == False:
+                
+            if check_number > 2: #資料超過兩筆才繪製，避免只有顯示起點終點車站的車次被繪入
+                path = 'M'
+                i = 0
+                while True:
+                    if self.stations_loc.__contains__(train_time_space.iloc[i, 1]):
+                        x = round(train_time_space.iloc[i, 2] * 10 + 50, 4)
+                        y = round(self.stations_loc[train_time_space.iloc[i, 1]] + 50, 4)
+                        path += str(x) + ',' + str(y) + ' '
+                        
+                    i += 1
+
+                    if i == len(train_time_space.index):
+                        break
+                
+                self.draw_line(train_id, path, color, '')
+
+
+    
+    #繪製線條
+    def draw_line(self, train_id, path, color, midnight_id):
+        # print(path)
+        line_id = ''
+        #如果跨午夜車次，車次線ID修改，避免跨午夜車次線無車次號的問題
+        if midnight_id == '':
+            line_id = train_id
+        elif midnight_id != '':
+            line_id = midnight_id
+            
+        if path != 'M': #避免無資料
+            self.dwg.add(self.dwg.path(id = line_id, d = path, class_=color))
+            for i in range(0, 6):
+                text = self.dwg.add(svgwrite.text.Text(""))
+                text.add(svgwrite.text.TextPath(path='#'+line_id, text=train_id, startOffset=50 + 600 * i, method='align', spacing='exact', class_=color))
+
+    #存檔
+    def save(self):
+        self.dwg.save()
+        return dict_line_kind[self.line] + ' 日期：' + self.date + ' 運行圖繪製完成 \n'
